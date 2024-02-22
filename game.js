1eb2a10ea92f14b6f8685f063e3c0cbcd755d312
JulianDominic <juliandominic@yahoo.com.sg>
2024-02-22 12:46:16 +0800

    Implemented playGame() function with a for-loop

--- game.js
+++ game.js
@@ -6,43 +6,63 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    // Function returns the victory string, player score, and computer score
     playerSelection = playerSelection.toLowerCase()
     computerSelection = computerSelection.toLowerCase()
 
     // Player chooses rock
     if (playerSelection == "rock" && computerSelection == "rock") {
-        return "It's a tie! Rock and Rock";
+        return ["It's a tie! Rock and Rock", 0, 0];
     }
     else if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You lose! Paper beats Rock";
+        return ["You lose! Paper beats Rock", 0, 1];
     }
     else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return "You win! Rock beats Scissors";
+        return ["You win! Rock beats Scissors", 1, 0];
     }
 
     // Player chooses paper
     else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You win! Paper beats Rock";
+        return ["You win! Paper beats Rock", 1, 0];
     }
     else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "It's a tie! Paper and Paper";
+        return ["It's a tie! Paper and Paper", 0, 0];
     }
     else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lose! Scissors beats Paper";
+        return ["You lose! Scissors beats Paper", 0, 1];
     }
 
     // Player chooses scissors
     else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You lose! Rock beats Scissors";
+        return ["You lose! Rock beats Scissors", 0, 1];
     }
     else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win! Scissors beats Paper";
+        return ["You win! Scissors beats Paper", 1, 0];
     }
     else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "It's a tie! Scissors and Scissors";
+        return ["It's a tie! Scissors and Scissors", 0, 0];
     }
 }
 
-const playerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors'")
-const computerSelection = getComputerChoice()
-console.log(playRound(playerSelection, computerSelection))
+// Loop method
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Choose 'Rock', 'Paper' or 'Scissors'");
+        const computerSelection = getComputerChoice();
+        let results = playRound(playerSelection, computerSelection);
+        playerScore += results[1];
+        computerScore += results[2];
+    }
+
+    if (playerScore > computerScore) {
+        return "Winnner! You won the most games"
+    }
+    else {
+        return "Loser! The computer won the most games"
+    }
+}
+
+console.log(playGame())
